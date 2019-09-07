2e111c5ab02b4c84d919f1a4ecd37199d5f72054
Petar Pejovic <petarpejovic1997@gmail.com>
2019-09-07 13:45:57 +0200

    First commit

---
+++ ChatApp/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatApp.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChatApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddCors(options => options.AddPolicy("CorsPolicy", builder =>
+            {
+                builder.AllowAnyMethod().AllowAnyHeader()
+                    .WithOrigins("https://localhost:44349/")
+                    .AllowCredentials();
+            }));
+            services.AddSignalR();
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.Stores.MaxLengthForKeys = 128)
+            .AddEntityFrameworkStores<IdentityDbContext>()
+            .AddDefaultUI()
+            .AddDefaultTokenProviders();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseCors("PolicyCors");
+            app.UseSignalR(route =>
+            {
+                route.MapHub<ChatHub>("/chatHub");
+            });
+            app.UseMvc();
+        }
+    }
+}
